477a2fa1a0789ec3b7144efbdca412cf03342b3c
Stefan Taubert <stefan.taubert@posteo.de>
2020-06-11 12:53:04 +0200

    adjusting C17 script bugfixing

--- thchs_no_tone_C17_synthesize.sh
+++ thchs_no_tone_C17_synthesize.sh
@@ -1,8 +1,8 @@
 base_dir='/datasets/models/taco2pt_ms'
 waveglow='/datasets/models/pretrained/waveglow_256channels_universal_v5.pt'
-ds_name='thchs'
+ds_name='thchs_no_tone'
 speaker='C17'
-checkpoint_name='2999'
+checkpoint_name='100'
 checkpoint=${ds_name}_${speaker}_ipa_${checkpoint_name}
 
 python paths.py --base_dir=$base_dir
@@ -12,10 +12,6 @@ dest_model_path=${base_dir}/saved_checkpoints/${checkpoint}
 if [ ! -f $dest_model_path ]; then
   cp $model_path $dest_model_path
 fi
-
-python script_txt_pre.py --base_dir=$base_dir --ipa='true' --text='examples/chn_ipa.txt' --is_ipa='true' --speaker=$speaker --ds_name=$ds_name --speaker=$speaker
-
-python synthesize.py --base_dir=$base_dir --checkpoint=$checkpoint --output_name=chn_${ds_name}_${speaker}_${checkpoint_name} --waveglow=$waveglow --ds_name=$ds_name --speaker=$speaker --hparams='sampling_rate=18000'
 
 python script_txt_pre.py --base_dir=$base_dir --ipa='true' --text='examples/north_chn.txt' --is_ipa='true' --speaker=$speaker --ds_name=$ds_name --speaker=$speaker --map='maps/en_chn.txt'
 
