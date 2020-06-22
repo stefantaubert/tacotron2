b239504f50f78410c4e99b66ee46939797d0e772
Stefan Taubert <stefan.taubert@posteo.de>
2020-06-22 08:49:31 +0200

    synthesized with trained model after 19999 steps: audio quality 0/5

--- start_thchs_toneless_ipa_inference.sh
+++ start_thchs_toneless_ipa_inference.sh
@@ -1,5 +1,5 @@
 base_dir='/datasets/models/taco2pt_v2'
-training_dir='training_2020-06-19_13-47-04'
+training_dir='thchs_uni_map_from_scratch'
 
 #python run.py --base_dir=$base_dir --training_dir=$training_dir --debug='false' --mode='infer' --config='configs/thchs_toneless_ipa/chn_ipa.json'
 
