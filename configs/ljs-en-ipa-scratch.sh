493c078498bad3158b5db038e9309eb4f63c8355
Stefan Taubert <stefan.taubert@posteo.de>
2020-07-15 10:37:14 +0200

    - bugfix trainsize and valsize parameter type - refactored ljs scratch script

--- configs/ljs-en-ipa-scratch.sh
+++ configs/ljs-en-ipa-scratch.sh
@@ -8,8 +8,9 @@ conda activate test1
 export base_dir="/datasets/models/taco2pt_v2"
 export datasets_dir="/datasets"
 export pretrained_dir="/datasets/models/pretrained"
-export custom_training_name="ljs_ipa_ms_from_scratch"
 export waveglow="$pretrained_dir/waveglow_256channels_universal_v5.pt"
+# Custom for this model
+export custom_training_name="ljs_ipa_ms_from_scratch"
 export ds_name="ljs_ipa_v2"
 export speakers="$ds_name,all"
 export batch_size=26
@@ -21,8 +22,9 @@ source activate taco2pytorch
 export base_dir="/home/stefan_taubert/taco2pt_v2"
 export datasets_dir="/home/stefan_taubert/datasets"
 export pretrained_dir="$base_dir/pretrained"
-export custom_training_name="ljs_ipa_ms_from_scratch"
 export waveglow="$pretrained_dir/waveglow_256channels_universal_v5.pt"
+# Custom for this model
+export custom_training_name="ljs_ipa_ms_from_scratch"
 export ds_name="ljs_ipa"
 export speakers="$ds_name,all"
 export batch_size=52
@@ -33,15 +35,16 @@ conda activate test2
 export base_dir="/home/stefan/taco2pt_v2"
 export datasets_dir="/home/stefan/datasets"
 export pretrained_dir="$base_dir/pretrained"
-export custom_training_name="ljs_ipa_ms_from_scratch"
 export waveglow="$pretrained_dir/waveglow_256channels_universal_v5.pt"
+# Custom for this model
+export custom_training_name="ljs_ipa_ms_from_scratch"
 export ds_name="ljs_ipa"
 export speakers="$ds_name,all"
 export batch_size=26
 
 
 # Preprocessing
-python script_ljs_pre.py \
+python -m script_ljs_pre \
   --base_dir=$base_dir \
   --data_dir="$datasets_dir/LJSpeech-1.1" \
   --ipa \
@@ -50,52 +53,57 @@ python script_ljs_pre.py \
   --auto_dl \
   --no_debugging
 
+
 # Training from scratch
 export hparams="batch_size=$batch_size,iters_per_checkpoint=500,epochs=500"
-python paths.py --base_dir=$base_dir --custom_training_name=$custom_training_name --no_debugging
-python script_train.py --base_dir=$base_dir --training_dir=$custom_training_name --speakers=$speakers --hparams=$hparams --no_debugging
-#python -m script_train --base_dir=$base_dir --training_dir=$custom_training_name --speakers=$speakers --hparams=$hparams --no_debugging
+python -m paths --base_dir=$base_dir --custom_training_name=$custom_training_name --no_debugging
+python -m script_train --base_dir=$base_dir --training_dir=$custom_training_name --speakers=$speakers --hparams=$hparams --train_size=0.9 --validation_size=1.0 --no_debugging
+
 
 ## Continue training
 export hparams="batch_size=$batch_size,iters_per_checkpoint=500,epochs=500"
-python script_train.py --base_dir=$base_dir --training_dir=$custom_training_name --hparams=$hparams --continue_training --no_debugging
+python -m script_train --base_dir=$base_dir --training_dir=$custom_training_name --hparams=$hparams --continue_training --no_debugging
+
 
 # Inference
-python script_dl_waveglow_pretrained.py --pretrained_dir=$pretrained_dir --no_debugging
+python -m script_dl_waveglow_pretrained --pretrained_dir=$pretrained_dir --no_debugging
 export text_map="maps/inference/en_v1.json"
 export speaker="ljs_ipa,1"
 
 export text="examples/ipa/north_sven_orig.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ipa --ignore_tones --ignore_arcs  --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=81500 --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ipa --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=81500 --map=$text_map --no_debugging
 
 export text="examples/chn/thchs.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=chn --ignore_tones --ignore_arcs  --speaker=$speaker --waveglow=$waveglow --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=chn --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --map=$text_map --no_debugging
 
 export text="examples/ger/example.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ger --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ger --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
 
 export text="examples/ger/nord.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ger --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ger --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
 
 export text="examples/ipa/north_sven_v2.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ipa --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ipa --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --map=$text_map --no_debugging
 
 export text="examples/en/north.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=en --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=en --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
 
 export text="examples/en/democritus_v2.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=en --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=en --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
 
 export text="examples/ipa/north_ger.txt"
-python script_inference.py --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ipa --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
+python -m script_inference --base_dir=$base_dir --training_dir=$custom_training_name --ipa --text=$text --lang=ipa --ignore_tones --ignore_arcs --speaker=$speaker --waveglow=$waveglow --custom_checkpoint=79000 --map=$text_map --no_debugging
 
 # Validate checkpoints
 export hparams="batch_size=52"
 export select_pattern=10000
-python script_eval_checkpoints.py --base_dir=$base_dir --training_dir=$custom_training_name --hparams=$hparams --select=2000 --min=70000 --no_debugging
+python -m script_eval_checkpoints --base_dir=$base_dir --training_dir=$custom_training_name --hparams=$hparams --select=2000 --min=70000 --no_debugging
 
 # Plot Embeddings
-python plot_embeddings.py --base_dir=$base_dir --training_dir=$custom_training_name --no_debugging
+python -m plot_embeddings \
+  --base_dir=$base_dir \
+  --training_dir=$custom_training_name \
+  --no_debugging
 
 # Create Inference Map
 python script_create_map_template.py \
@@ -107,7 +115,7 @@ python script_create_map_template.py \
   --no_debugging
 
 # Update Inference Map
-python script_create_map_template.py \
+python -m script_create_map_template \
   --a="$base_dir/ljs_ipa_ms_from_scratch/filelist/symbols.json" \
   --b="examples/ipa/corpora.txt" \
   --existing_map="maps/inference/en_v1.json"
