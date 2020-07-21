17df524b8696542d94a9701c3f1b1e15988bcdb4
Stefan Taubert <stefan.taubert@posteo.de>
2020-07-21 17:15:53 +0200

    - implemented: inference for waveglow - utterances from validation set can be inferred with waveglow now

--- src/waveglow/inference.py
+++ src/waveglow/inference.py
@@ -25,60 +25,114 @@
 #
 # *****************************************************************************
 import os
+from pathlib import Path
+from shutil import copyfile
+import matplotlib
+matplotlib.use("Agg")
+
+import matplotlib.pylab as plt
 from scipy.io.wavfile import write
+
 import torch
-from src.waveglow.mel2samp import files_to_list, MAX_WAV_VALUE
+from src.tacotron.script_plot_mel import (Mel2Samp, get_audio, get_segment,
+                                          plot_melspec,
+                                          stack_images_vertically)
 from src.waveglow.denoiser import Denoiser
+from src.waveglow.hparams import create_hparams
+from src.waveglow.mel2samp import MAX_WAV_VALUE
+from src.waveglow.train import get_checkpoint_dir, load_model
 
 
-def main(mel_files, waveglow_path, sigma, output_dir, sampling_rate, is_fp16,
-     denoiser_strength):
-  mel_files = files_to_list(mel_files)
-  waveglow = torch.load(waveglow_path)['model']
-  waveglow = waveglow.remove_weightnorm(waveglow)
-  waveglow.cuda().eval()
-  if is_fp16:
-    from apex import amp
-    waveglow, _ = amp.initialize(waveglow, [], opt_level="O3")
+def infer(training_dir_path: str, infer_dir_path: str, hparams, checkpoint: str, infer_wav_path: str, denoiser_strength: float, sigma: float):
+  hparams = create_hparams(hparams)
+  plotter = Mel2Samp(hparams)
+
+  checkpoint_path = os.path.join(get_checkpoint_dir(training_dir_path), checkpoint)
+  print("Using model:", checkpoint_path)
+  assert os.path.isfile(checkpoint_path)
+  checkpoint_dict = torch.load(checkpoint_path, map_location='cpu')
+  model_state_dict = checkpoint_dict['state_dict']
+  model = load_model(hparams)
+  model.load_state_dict(model_state_dict)
+  model = model.remove_weightnorm(model)
+  model.cuda().eval()
+
+  # if is_fp16:
+  #   from apex import amp
+  #   waveglow, _ = amp.initialize(waveglow, [], opt_level="O3")
 
   if denoiser_strength > 0:
-    denoiser = Denoiser(waveglow).cuda()
+    denoiser = Denoiser(model).cuda()
 
-  for i, file_path in enumerate(mel_files):
-    file_name = os.path.splitext(os.path.basename(file_path))[0]
-    mel = torch.load(file_path)
-    mel = torch.autograd.Variable(mel.cuda())
-    mel = torch.unsqueeze(mel, 0)
-    mel = mel.half() if is_fp16 else mel
-    with torch.no_grad():
-      audio = waveglow.infer(mel, sigma=sigma)
-      if denoiser_strength > 0:
-        audio = denoiser(audio, denoiser_strength)
-      audio = audio * MAX_WAV_VALUE
-    audio = audio.squeeze()
-    audio = audio.cpu().numpy()
-    audio = audio.astype('int16')
-    audio_path = os.path.join(
-      output_dir, "{}_synthesis.wav".format(file_name))
-    write(audio_path, sampling_rate, audio)
-    print(audio_path)
+  print("Inferring {}...".format(infer_wav_path))
+
+  a = get_audio(infer_wav_path)
+  mel = plotter.get_mel(a)
+  
+  #mel = torch.load(file_path)
+  mel = torch.autograd.Variable(mel.cuda())
+  mel = torch.unsqueeze(mel, 0)
+  #mel = mel.half() if is_fp16 else mel
+
+  with torch.no_grad():
+    audio = model.infer(mel, sigma=sigma)
+    if denoiser_strength > 0:
+      audio = denoiser(audio, denoiser_strength)
+    audio = audio * MAX_WAV_VALUE
+  audio = audio.squeeze()
+  audio = audio.cpu().numpy()
+  audio = audio.astype('int16')
+  #audio_path = os.path.join(output_dir, "{}_synthesis.wav".format(file_name))
+
+  last_dir_name = Path(infer_dir_path).parts[-1]
+  output_name = "{}".format(last_dir_name)
+  out_path_template = os.path.join(infer_dir_path, output_name)
+  path_original_wav = "{}_orig.wav".format(out_path_template)
+  path_original_plot = "{}_orig.png".format(out_path_template)
+  path_inferred_wav = "{}_inferred.wav".format(out_path_template)
+  path_inferred_plot = "{}_inferred.png".format(out_path_template)
+  path_compared_plot = "{}_comparison.png".format(out_path_template)
+
+  write(path_inferred_wav, hparams.sampling_rate, audio)
+
+  print("Plotting...")
+  wav_inferred = get_audio(path_inferred_wav)
+  wav_orig = get_audio(infer_wav_path)
+  #wav = get_segment(wav)
+  mel_inferred = plotter.get_mel(wav_inferred)
+  mel_orig = plotter.get_mel(wav_orig)
+
+  plot_melspec(mel_inferred, title="Inferred")
+  plt.savefig(path_inferred_plot, bbox_inches='tight')
+  plot_melspec(mel_orig, title="Original")
+  plt.savefig(path_original_plot, bbox_inches='tight')
+
+  stack_images_vertically([path_original_plot, path_inferred_plot], path_compared_plot)
+
+  # plot_melspec([mel_orig, mel_inferred], titles=["Original", "Inferred"])
+  # plt.savefig(path_compared_plot, bbox_inches='tight')
+  copyfile(infer_wav_path, path_original_wav)
+  print("Finished.")
 
 
 if __name__ == "__main__":
   import argparse
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('-f', "--filelist_path", required=True)
-  parser.add_argument('-w', '--waveglow_path',
-            help='Path to waveglow decoder checkpoint with model')
-  parser.add_argument('-o', "--output_dir", required=True)
-  parser.add_argument("-s", "--sigma", default=1.0, type=float)
-  parser.add_argument("--sampling_rate", default=22050, type=int)
-  parser.add_argument("--is_fp16", action="store_true")
-  parser.add_argument("-d", "--denoiser_strength", default=0.0, type=float,
-            help='Removes model bias. Start with 0.1 and adjust')
+  # parser.add_argument('-f', "--filelist_path", required=True)
+  # parser.add_argument('-f', "--filelist_path", required=True)
+  # parser.add_argument('-w', '--waveglow_path',
+  #           help='Path to waveglow decoder checkpoint with model')
+  # parser.add_argument('-o', "--output_dir", required=True)
+  # parser.add_argument("-s", "--sigma", default=1.0, type=float)
+  # parser.add_argument("--sampling_rate", default=22050, type=int)
+  # parser.add_argument("--is_fp16", action="store_true")
+  # parser.add_argument("-d", "--denoiser_strength", default=0.0, type=float,
+  #           help='Removes model bias. Start with 0.1 and adjust')
 
   args = parser.parse_args()
 
-  main(args.filelist_path, args.waveglow_path, args.sigma, args.output_dir,
-     args.sampling_rate, args.is_fp16, args.denoiser_strength)
+
+
+  # main(args.filelist_path, args.waveglow_path, args.sigma, args.output_dir,
+  #    args.sampling_rate, args.is_fp16, args.denoiser_strength)
---
+++ src/waveglow/script_validate.py
@@ -0,0 +1,78 @@
+import argparse
+import json
+import os
+import random
+from shutil import copyfile
+
+import pandas as pd
+
+from src.common.train_log import reset_log
+from src.common.utils import (csv_separator, get_speaker_count_csv,
+                              get_utterance_names_csv, parse_ds_speaker,
+                              speaker_id_col, speaker_name_col,
+                              symbols_str_col, utt_name_col, wavpath_col)
+from src.script_paths import (ds_preprocessed_file_name,
+                              ds_preprocessed_symbols_name, filelist_file_name,
+                              filelist_symbols_file_name,
+                              filelist_validation_file_name, get_ds_dir,
+                              get_filelist_dir, get_inference_dir,
+                              get_validation_dir, inference_config_file,
+                              log_inference_config, log_input_file,
+                              log_map_file, log_train_config, log_train_map,
+                              train_config_file)
+from src.waveglow.prepare_ds import load_filepaths
+from src.waveglow.train import get_last_checkpoint
+from src.waveglow.inference import infer
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--no_debugging', action='store_true')
+  parser.add_argument('--base_dir', type=str, help='base directory')
+  parser.add_argument('--training_dir', type=str)
+  parser.add_argument('--utterance', type=str, help="Utterance name or random-val or random-val-B12")
+  parser.add_argument('--hparams', type=str)
+  parser.add_argument("--denoiser_strength", default=0.0, type=float, help='Removes model bias. Start with 0.1 and adjust')
+  parser.add_argument("--sigma", default=1.0, type=float)
+  parser.add_argument('--custom_checkpoint', type=str)
+
+  args = parser.parse_args()
+
+  if not args.no_debugging:
+    args.base_dir = '/datasets/models/taco2pt_v2'
+    args.training_dir = 'wg_debug'
+    args.utterance = "LJ001-0001"
+    #args.utterance = "random-val"
+
+  training_dir_path = os.path.join(args.base_dir, args.training_dir)
+
+  if args.custom_checkpoint:
+    checkpoint = args.custom_checkpoint
+  else:
+    checkpoint = get_last_checkpoint(training_dir_path)
+
+  infer_utterance_name = args.utterance
+
+  if "random-val" in infer_utterance_name:
+    tmp = infer_utterance_name.split('-')
+    speaker_is_given = len(tmp) == 3
+    if speaker_is_given:
+      speaker = tmp[2]
+
+    valset_path = os.path.join(get_filelist_dir(training_dir_path), filelist_validation_file_name)
+    all_names = list(load_filepaths(valset_path))
+    infer_utterance_name = random.choice(all_names)[0]
+    while speaker_is_given:
+      if speaker in infer_utterance_name:
+        break
+      infer_utterance_name = random.choice(all_names)
+    print("Selected random validationset utterance: {}".format(infer_utterance_name))
+  else:
+    valset_path = os.path.join(get_filelist_dir(training_dir_path), filelist_validation_file_name)
+    all_names = list(load_filepaths(valset_path))
+    for x in all_names:
+      if infer_utterance_name in x[0]:
+        infer_utterance_name = x[0]
+  basename = os.path.basename(infer_utterance_name)[:-4]
+  infer_dir_path = get_validation_dir(training_dir_path, basename, checkpoint, '')
+
+  infer(training_dir_path, infer_dir_path, hparams=args.hparams, checkpoint=checkpoint, infer_wav_path=infer_utterance_name, denoiser_strength=args.denoiser_strength, sigma=args.sigma)
